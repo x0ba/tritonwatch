bf190047a1ce725e03d3b115eacfaf9ec976b0b9
Daniel Xu <hi@danielx.me>
2026-08-25 23:42:34 -0700

    add boilerplate for services

---
+++ services/watchlist-service/src/test/java/app/tritonwatch/watchlist_service/WatchlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.tritonwatch.watchlist_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
